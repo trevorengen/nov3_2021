d25ac41dd60f6e3ea21908e725b62a918ef588ab
Trevor Engen <48308407+trevorengen@users.noreply.github.com>
2021-11-03 12:31:19 -0700

    Add quick union and quick find versions.

--- unionFind.js
+++ unionFind.js
@@ -1,4 +1,5 @@
-class UnionFind {
+// QUICK FIND VERSION.
+class UnionQuickFind {
     constructor(size) {
         this.root = [];
         for (let i=0; i<size; i++) {
@@ -19,6 +20,36 @@ class UnionFind {
                     this.root[i] = rootX;
                 }
             }
+        }
+    }
+
+    connected(x,y) {
+        return this.find(x) === this.find(y);
+    }
+}
+
+
+// QUICK UNION VERSION
+class QuickUnionFind {
+    constructor(size) {
+        this.root = [];
+        for (let i=0; i<size; i++) {
+            this.root.push(i);
+        }
+    }
+
+    find(x) {
+        while (x !== this.root[x]) {
+            x = this.root[x];
+        }
+        return x;
+    }
+
+    union(x,y) {
+        rootX = this.find(x);
+        rootY = this.find(y);
+        if (rootX !== rootY) {
+            this.root[rootY] = rootX;
         }
     }
 
